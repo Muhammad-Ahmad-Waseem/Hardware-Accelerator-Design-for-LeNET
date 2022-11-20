70ab540da92bc64cd3fb3b1a0d171735f9f17eb5
Muhammad Ahmad Waseem <ahmadwaseem648@gmail.com>
2022-11-20 18:19:50 +0500

    Add files via upload

---
+++ Semester_project.ip_user_files/ip/MUL/MUL_stub.v
@@ -0,0 +1,23 @@
+// Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2017.4 (win64) Build 2086221 Fri Dec 15 20:55:39 MST 2017
+// Date        : Tue Jan 25 23:49:06 2022
+// Host        : EE-114103LT running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub
+//               c:/Users/m_waseem/Semester_project/Semester_project.srcs/sources_1/ip/MUL/MUL_stub.v
+// Design      : MUL
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a100tcsg324-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "mult_gen_v12_0_13,Vivado 2017.4" *)
+module MUL(CLK, A, B, P)
+/* synthesis syn_black_box black_box_pad_pin="CLK,A[15:0],B[15:0],P[31:0]" */;
+  input CLK;
+  input [15:0]A;
+  input [15:0]B;
+  output [31:0]P;
+endmodule
