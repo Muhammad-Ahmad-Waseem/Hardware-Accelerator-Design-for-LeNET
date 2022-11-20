c75726f5257cef1d70d20f9259b1aa97ccd175c7
Muhammad Ahmad Waseem <ahmadwaseem648@gmail.com>
2022-11-20 18:23:18 +0500

    Add files via upload

---
+++ Semester_project.ip_user_files/sim_scripts/MUL/ies/run.f
@@ -0,0 +1,25 @@
+-makelib ies_lib/xil_defaultlib -sv \
+  "C:/Windows/Vivado/2017.4/data/ip/xpm/xpm_memory/hdl/xpm_memory.sv" \
+-endlib
+-makelib ies_lib/xpm \
+  "C:/Windows/Vivado/2017.4/data/ip/xpm/xpm_VCOMP.vhd" \
+-endlib
+-makelib ies_lib/xbip_utils_v3_0_8 \
+  "../../../ipstatic/hdl/xbip_utils_v3_0_vh_rfs.vhd" \
+-endlib
+-makelib ies_lib/xbip_pipe_v3_0_4 \
+  "../../../ipstatic/hdl/xbip_pipe_v3_0_vh_rfs.vhd" \
+-endlib
+-makelib ies_lib/xbip_bram18k_v3_0_4 \
+  "../../../ipstatic/hdl/xbip_bram18k_v3_0_vh_rfs.vhd" \
+-endlib
+-makelib ies_lib/mult_gen_v12_0_13 \
+  "../../../ipstatic/hdl/mult_gen_v12_0_vh_rfs.vhd" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  "../../../../Semester_project.srcs/sources_1/ip/MUL/sim/MUL.vhd" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  glbl.v
+-endlib
+
